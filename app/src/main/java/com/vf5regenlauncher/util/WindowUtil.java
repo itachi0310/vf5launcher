e30c48ea4d0682ef892126ac3588d717d9f80ff6
Andrew Chu <chu.tuananh0310@gmail.com>
2026-09-10 23:58:10 +0700

    đang test
    đang test

--- app/src/main/java/com/vf5regenlauncher/util/WindowUtil.java
+++ app/src/main/java/com/vf5regenlauncher/util/WindowUtil.java
@@ -126,6 +126,10 @@ public class WindowUtil {
                     return;
                 }
                 intent = FytPackage.getIntent(LauncherApplication.sApp, AAppPackageNmae);
+                if (intent == null) {
+                    Log.e("LZP", "Không tìm thấy Intent cho package: " + AAppPackageNmae);
+                    return;
+                }
                 if (AAppPackageNmae.equals("com.syu.camera360")) {
                     AndrewLauncherActivity.getInstance().sendBroadcast(new Intent("com.syu.camera360.show"));
                 }
@@ -176,6 +180,10 @@ public class WindowUtil {
                     return;
                 }
                 intent = FytPackage.getIntent(LauncherApplication.sApp, AppPackageNmae);
+                if (intent == null) {
+                    Log.e("LZP", "Không tìm thấy Intent cho package: " + AppPackageNmae);
+                    return;
+                }
                 if (AppPackageNmae.equals("com.syu.camera360")) {
                     AndrewLauncherActivity.getInstance().sendBroadcast(new Intent("com.syu.camera360.show"));
                 }
